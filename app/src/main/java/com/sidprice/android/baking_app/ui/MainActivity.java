a3b50acad68a31ed83a306a4883769f7ebb26e5a
Sid Price <sid@sidprice.com>
2018-07-25 09:54:45 -0600

    Added RecyclerView to main activity

--- app/src/main/java/com/sidprice/android/baking_app/ui/MainActivity.java
+++ app/src/main/java/com/sidprice/android/baking_app/ui/MainActivity.java
@@ -1,8 +1,11 @@
 package com.sidprice.android.baking_app.ui;
 
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
 import com.sidprice.android.baking_app.R;
@@ -11,10 +14,17 @@ import com.sidprice.android.baking_app.data.RecipesViewModel;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    private RecyclerView    mCardRecyclerView ;
+    private Context         mContext ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mContext = getBaseContext() ;
         setContentView(R.layout.activity_main);
+        //
+        mCardRecyclerView = (RecyclerView)findViewById(R.id.cards_recycler_view) ;
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(mContext) ;
+        mCardRecyclerView.setLayoutManager(linearLayoutManager);
 
         RecipesViewModel    recipesViewModel = ViewModelProviders.of(this).get(RecipesViewModel.class) ;
         recipesViewModel.getRecipes().observe(this, recipes -> {
